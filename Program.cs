fd95746ad37e49600b074809f25837740a3d09c7
JoaoPedrocordoba <jp.8pedro862gmail.com>
2022-05-14 10:58:05 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int n1, n2, Contador;
+
+Console.Write("Início:");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Fim:");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n2 < n1)
+{
+    Console.WriteLine("Erro");
+}
+
+else
+{
+    Contador = n1;
+    while( Contador < n2 )
+    {
+        Console.WriteLine(Contador);
+        Contador = Contador + 1;
+    }
+}
